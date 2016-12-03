f90353993f764a3b2557b590404c0adfedb1b2fd
Andrew Conroy <aconroy@longwintermembers.com>
2016-12-03 15:46:58 -0700

    added nodes to list

--- scripts/LWS_InstallScripts.py
+++ scripts/LWS_InstallScripts.py
@@ -21,7 +21,7 @@ def installScripts():
     updateVersion = False
     scripts = cmds.internalVar(usd = True)
     scriptList = os.listdir(scripts)
-    lwsScripts = ['LWS_SpeedPick.pyc','LWS_Install.pyc','LWS_Menu.mel','LWS_LicenseUI.pyc']
+    lwsScripts = ['LWS_SpeedPick.pyc','LWS_Install.pyc','LWS_Menu.mel','LWS_LicenseUI.pyc','MayaNodes.mel']
     for script in lwsScripts:
         if script not in scriptList :
             updateVersion = True
