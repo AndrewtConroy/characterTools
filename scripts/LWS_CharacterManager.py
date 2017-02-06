ff24ebc12e5da57f52edbcb70b1031100ee79030
Andrew Conroy <aconroy@longwintermembers.com>
2017-02-06 16:43:53 -0700

    new char link

--- scripts/LWS_CharacterManager.py
+++ scripts/LWS_CharacterManager.py
@@ -155,7 +155,7 @@ class BasicDialog (gui.QDialog):
 
     def downloadCharacters(self):
         try:
-            url = 'https://www.dropbox.com/s/454vnvmw5c9k78d/LWS_Characters.zip?dl=1'
+            url = 'https://www.dropbox.com/s/0ri5enj90ce7big/LWS_Characters.zip?dl=1'
             hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
                    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
